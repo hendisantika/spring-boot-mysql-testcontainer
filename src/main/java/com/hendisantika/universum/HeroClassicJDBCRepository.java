1675502c853c810f7d2a8bf8fd8d983b56bf78c2
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 08:56:53 +0700

    #11 - Add allHeros in HeroClassicJDBCRepository :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/universum/HeroClassicJDBCRepository.java
+++ src/main/java/com/hendisantika/universum/HeroClassicJDBCRepository.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +28,12 @@ public class HeroClassicJDBCRepository {
     public void addHero(Hero hero) {
         jdbcTemplate.update("insert into hero (city, name, universum) values (?,?,?)",
                 hero.getCity(), hero.getName(), hero.getUniversum().name());
+    }
+
+    public Collection<Hero> allHeros() {
+        return jdbcTemplate.query("select * From hero",
+                (resultSet, i) -> new Hero(resultSet.getString("name"),
+                        resultSet.getString("city"),
+                        ComicUniversum.valueOf(resultSet.getString("universum"))));
     }
 }
