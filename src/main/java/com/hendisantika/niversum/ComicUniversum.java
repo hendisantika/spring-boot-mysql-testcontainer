6ce4dd51249e2ba4b7fffabeac88a0c30e3008ad
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 08:53:55 +0700

    #7 - Add ComicUniversum Enum :pencil2: :green_book: :whale:

---
+++ src/main/java/com/hendisantika/niversum/ComicUniversum.java
@@ -0,0 +1,26 @@
+package com.hendisantika.niversum;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mysql-testcontainer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/3/22
+ * Time: 08:53
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ComicUniversum {
+    MARVEL("Marvel"),
+    DC_COMICS("DC Comic");
+
+    private final String displayName;
+
+    ComicUniversum(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
