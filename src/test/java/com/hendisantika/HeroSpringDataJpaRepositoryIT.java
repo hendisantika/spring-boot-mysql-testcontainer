c7342a8d21c9fea9423a13e001be1f58c067bb23
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 09:28:19 +0700

    #45 - Add findHeroesBySearchCriteria Test Case in HeroSpringDataJpaRepositoryIT :bowtie: :whale:

--- src/test/java/com/hendisantika/HeroSpringDataJpaRepositoryIT.java
+++ src/test/java/com/hendisantika/HeroSpringDataJpaRepositoryIT.java
@@ -1,12 +1,19 @@
 package com.hendisantika;
 
+import com.hendisantika.universum.ComicUniversum;
+import com.hendisantika.universum.Hero;
 import com.hendisantika.universum.HeroSpringDataJpaRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +35,12 @@ class HeroSpringDataJpaRepositoryIT {
     @Autowired
     private HeroSpringDataJpaRepository repositoryUnderTest;
 
+    @Test
+    void findHeroesBySearchCriteria() {
+        repositoryUnderTest.save(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
+
+        Collection<Hero> heroes = repositoryUnderTest.findHerosBySearchCriteria("Batman");
+
+        assertThat(heroes).hasSize(1).contains(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
+    }
 }
