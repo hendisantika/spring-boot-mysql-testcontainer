d20a44cd248663415375b679abd4cd53eba4193e
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 09:27:16 +0700

    #43 - Add databaseProperties in HeroRestControllerIT :bowtie: :whale:

--- src/test/java/com/hendisantika/HeroRestControllerIT.java
+++ src/test/java/com/hendisantika/HeroRestControllerIT.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -49,5 +51,12 @@ class HeroRestControllerIT {
         mockMvc.perform(get("/heros"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[*].name", containsInAnyOrder("Batman", "Superman")));
+    }
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
     }
 }
