f5a86e3313a9bfd487a6bc3b72f28dee3aeeb1c9
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 09:16:37 +0700

    #30 - Add DatabaseBaseTest :bowtie: :whale:

---
+++ src/test/java/com/hendisantika/DatabaseBaseTest.java
@@ -0,0 +1,30 @@
+package com.hendisantika;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mysql-testcontainer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/3/22
+ * Time: 09:15
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class DatabaseBaseTest {
+    static final MySQLContainer DATABASE = new MySQLContainer();
+
+    static {
+        DATABASE.start();
+    }
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", DATABASE::getJdbcUrl);
+        registry.add("spring.datasource.username", DATABASE::getUsername);
+        registry.add("spring.datasource.password", DATABASE::getPassword);
+    }
+}
