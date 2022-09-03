75d920f5f89f140ab24dd8d9c6570fce51222460
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 09:29:42 +0700

    #48 - Add findHeroesBySearchCriteria in HeroSpringDataJpaRepositoryReuseDatabaseIT :bowtie: :whale:

--- src/test/java/com/hendisantika/HeroSpringDataJpaRepositoryReuseDatabaseIT.java
+++ src/test/java/com/hendisantika/HeroSpringDataJpaRepositoryReuseDatabaseIT.java
@@ -1,9 +1,16 @@
 package com.hendisantika;
 
+import com.hendisantika.universum.ComicUniversum;
+import com.hendisantika.universum.Hero;
 import com.hendisantika.universum.HeroSpringDataJpaRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,13 @@ class HeroSpringDataJpaRepositoryReuseDatabaseIT extends DatabaseBaseTest {
 
     @Autowired
     private HeroSpringDataJpaRepository repositoryUnderTest;
+
+    @Test
+    void findHeroesBySearchCriteria() {
+        repositoryUnderTest.save(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
+
+        Collection<Hero> heroes = repositoryUnderTest.findHerosBySearchCriteria("Batman");
+
+        assertThat(heroes).contains(new Hero("Batman", "Gotham City", ComicUniversum.DC_COMICS));
+    }
 }
