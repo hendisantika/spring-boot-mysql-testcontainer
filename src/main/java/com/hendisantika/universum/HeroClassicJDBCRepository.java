b573411619184433da542d739d5c84f6c1c5a7f8
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 08:56:03 +0700

    #9 - Add HeroClassicJDBCRepository :pencil2: :green_book: :whale:

---
+++ src/main/java/com/hendisantika/universum/HeroClassicJDBCRepository.java
@@ -0,0 +1,20 @@
+package com.hendisantika.universum;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mysql-testcontainer
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/3/22
+ * Time: 08:55
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public class HeroClassicJDBCRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+}
