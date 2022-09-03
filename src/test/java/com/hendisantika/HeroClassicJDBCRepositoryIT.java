5640a9a8611f2979d05253e5864eb9a030f3ba97
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 09:18:24 +0700

    #33 - Add dataSource in HeroClassicJDBCRepositoryIT :bowtie: :whale:

--- src/test/java/com/hendisantika/HeroClassicJDBCRepositoryIT.java
+++ src/test/java/com/hendisantika/HeroClassicJDBCRepositoryIT.java
@@ -3,6 +3,8 @@ package com.hendisantika;
 import com.hendisantika.universum.ComicUniversum;
 import com.hendisantika.universum.Hero;
 import com.hendisantika.universum.HeroClassicJDBCRepository;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import com.sun.istack.NotNull;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.ext.ScriptUtils;
@@ -10,6 +12,7 @@ import org.testcontainers.jdbc.JdbcDatabaseDelegate;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import javax.sql.DataSource;
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +44,14 @@ class HeroClassicJDBCRepositoryIT {
         Collection<Hero> heroes = repositoryUnderTest.allHeros();
 
         assertThat(heroes).hasSize(1);
+    }
+
+    @NotNull
+    private DataSource dataSource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(database.getJdbcUrl());
+        dataSource.setUser(database.getUsername());
+        dataSource.setPassword(database.getPassword());
+        return dataSource;
     }
 }
