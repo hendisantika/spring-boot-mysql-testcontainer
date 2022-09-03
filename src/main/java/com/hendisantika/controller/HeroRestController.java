b66ff35a8792bdc5d51e29c23cc7776ad6372132
Hendi Santika <hendisantika@yahoo.co.id>
2022-09-03 09:01:01 +0700

    #19 - Add allHeros in HeroRestController :pencil2: :green_book: :whale:

--- src/main/java/com/hendisantika/controller/HeroRestController.java
+++ src/main/java/com/hendisantika/controller/HeroRestController.java
@@ -1,6 +1,8 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.universum.Hero;
 import com.hendisantika.universum.HeroSpringDataJpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,5 +22,14 @@ public class HeroRestController {
 
     public HeroRestController(HeroSpringDataJpaRepository heroRepository) {
         this.heroRepository = heroRepository;
+    }
+
+    @GetMapping("heros")
+    public Iterable<Hero> allHeros(String searchCriteria) {
+        if (searchCriteria == null || searchCriteria.equals("")) {
+            return heroRepository.findAll();
+        }
+
+        return heroRepository.findHerosBySearchCriteria(searchCriteria);
     }
 }
