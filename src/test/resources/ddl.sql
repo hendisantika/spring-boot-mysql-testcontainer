53d13f607603042e3aefec35df75c647d946d586
hendisantika <hendisantika@yahoo.co.id>
2024-11-04 06:19:13 +0700

    fix: Integration Test :two:🫸🌀✏️📗🐧🐳⬆

--- src/test/resources/ddl.sql
+++ src/test/resources/ddl.sql
@@ -3,5 +3,11 @@ create table hero
     hero_id bigint AUTO_INCREMENT PRIMARY KEY,
     city      varchar(255),
     name      varchar(255),
-    universum varchar(255)
+--     universum varchar(255)
+    universum tinyint CHECK (universum BETWEEN 0 AND 1),
+) engine=InnoDB;
+
+CREATE TABLE hero_seq
+(
+    next_val bigint
 ) engine=InnoDB;
